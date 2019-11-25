0c403bf965be4052ab5dc6641118076b5d1acb71
Alex Li <aleksandr.li@nzme.co.nz>
2019-11-25 15:37:34 +1300

    Update version

---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿using OfficeOpenXml;
+using System;
+using System.IO;
+
+namespace JsonReplacer
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Reading File");
+
+            Console.WriteLine(@"Enter file path (e.g. C:\NZME.Deployments\sites\parameters\):");
+            string parametersFolderPath = Console.ReadLine();
+            try
+            {
+                JsonGenerate(parametersFolderPath);
+            }catch(Exception e)
+            {
+                Console.WriteLine($@"Error: {e.Message}. Press Any Key to quit...");
+                Console.ReadLine();
+            }
+            Console.WriteLine("Press Any Key to continue...");
+            Console.ReadLine();
+        }
+
+        static void JsonGenerate(string parametersFolderPath) {
+            var filePath = @".\Input\Values.xlsx";
+            FileInfo file = new FileInfo(filePath);
+
+            using (ExcelPackage package = new ExcelPackage(file)) {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+
+                for (int row = 2; row <= worksheet.Dimension.Rows; row++) {
+                    string json = File.ReadAllText(@".\Input\sample.json");
+                    dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+                    string frontOrBackoffice = "";
+                    string environment = "";
+                    string site = "";
+
+                    for (int col = 1; col <= worksheet.Dimension.Columns; col++)
+                    {
+                        string value = worksheet.Cells[row, col].Value.ToString();
+                        switch (col) 
+                        {
+                            case 1:
+                                jsonObj["parameters"]["actionGroupShortName"]["value"] = value;
+                                break;
+                            case 2:
+                                frontOrBackoffice = value;
+                                jsonObj["parameters"]["instanceName"]["value"] = value;
+                                break;
+                            case 3:
+                                environment = value;
+                                jsonObj["parameters"]["environment"]["value"] = value;
+                                break;
+                            case 4:
+                                jsonObj["parameters"]["location"]["value"] = value;
+                                break;
+                            case 5:
+                                jsonObj["parameters"]["pingTests"]["value"][0]["name"] = value;
+                                jsonObj["parameters"]["pingTests"]["value"][0]["syntheticMonitorName"] = value;
+                                jsonObj["parameters"]["pingTests"]["value"][0]["webTestName"] = value;
+                                jsonObj["parameters"]["pingTests"]["value"][0]["metricAlert"]["alertName"] = value;
+                                break;
+                            case 6:
+                                jsonObj["parameters"]["pingTests"]["value"][0]["webTestRequestUrl"] = value;
+                                break;
+                            case 7:
+                                jsonObj["parameters"]["pingTests"]["value"][0]["webTestExpectedHttpStatusCode"] = value;
+                                break;
+                            case 8:
+                                jsonObj["parameters"]["pingTests"]["value"][0]["webTestExpectedText"] = value;
+                                break;
+                            case 9:
+                                jsonObj["parameters"]["pingTests"]["value"][0]["applicationInsightsResourceName"] = value;
+                                break;
+                            case 10:
+                                jsonObj["parameters"]["pingTests"]["value"][0]["metricAlert"]["alertDescription"] = value;
+                                break;
+                            case 11:
+                                jsonObj["parameters"]["pingTests"]["value"][0]["metricAlert"]["alertSeverity"] = value;
+                                break;
+                            case 12:
+                                site = value;
+                                jsonObj["parameters"]["contextName"]["value"] = value;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    jsonObj["parameters"]["pingTests"]["value"][0]["webTestId"] = Guid.NewGuid().ToString();
+                    jsonObj["parameters"]["pingTests"]["value"][0]["webTestRequestId"] = Guid.NewGuid().ToString();
+                    string output = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj, Newtonsoft.Json.Formatting.Indented);
+
+                    Directory.CreateDirectory($@"{parametersFolderPath}\{environment}\{site}\{frontOrBackoffice}\");
+                    File.WriteAllText($@"{parametersFolderPath}\{environment}\{site}\{frontOrBackoffice}\azure-monitor-deploy.parameters.json", output);
+                    Console.WriteLine($@"Json successfully generated in {parametersFolderPath}\{environment}\{site}\{frontOrBackoffice}\azure-monitor-deploy.parameters.json");
+                }
+            }
+
+        }
+    }
+}
---
+++ bin/Debug/netcoreapp2.1/Readme.txt
@@ -0,0 +1,5 @@
+To generate params json file for alerts:
+1. Update spreadsheet values(Values.xlsx) in Input folder
+2. Run from RunExcelToJson.bat
+3. Enter path to parameters folder (e.g. C:\NZME.Deployments\sites\parameters\)
+3. Generated Files Can be found in parameters path for environments that have been setup in Excel spreadsheet(Values.xlsx)
